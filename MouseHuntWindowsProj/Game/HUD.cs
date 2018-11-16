865392bd8588ff79a3ebb0492820bbbae22f716e
Matthew Smith <matthew.smith@derivco.co.uk>
2018-11-16 12:26:39 +0000

    content change again

---
+++ MouseHuntWindowsProj/Game/HUD.cs
@@ -0,0 +1,27 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGame.Framework;
+#endregion
+
+namespace MouseHunt
+{
+
+    public class HUD
+    {
+        private int points;
+
+        protected void Update(GameTime gameTime)
+        {
+            
+        }
+
+        protected void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+         //       spriteBatch.Draw();
+        }
+    }
+}
